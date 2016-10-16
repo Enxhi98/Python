cee7bfc2557ecde167c0b5087ff41a174f1e131d
Enxhi98 <21321344@student.uwl.ac.uk>
2016-10-17 00:57:20 +0100

    Add files via upload

---
+++ Hi there(New File).py
@@ -0,0 +1,4 @@
+print("Hi there, what is your name?")
+Enxhi=input("Hi there, what is your name?")
+print("Hello" + Enxhi )
+print("How are you?")
---
+++ correct.py
@@ -0,0 +1,3 @@
+value = int(input("Type a number between 2 and 8:"))
+if (value>2) and (value<8):
+    print("That is correct")
