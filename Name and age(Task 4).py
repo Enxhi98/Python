176756757ded4e4d30d7cb729830397ff373c4ec
Enxhi98 <21321344@student.uwl.ac.uk>
2016-10-20 10:30:08 +0100

    Update Name and age(Task 4).py

--- Name and age(Task 4).py
+++ Name and age(Task 4).py
@@ -5,3 +5,7 @@ x = int
 x = int(input("And how old are you?"))
 y = int(x + 1)
 print("Hello" + Enxhi + ",next year you will be" + str(y) + "years old!")
+print("Oh thank you. Can you tell me how old was I three years ago?") # I added another question regarding my age three years ago this time using difference
+x= int(input("Yes of course. Could you tell me your age again?"))
+z = int(x - 3)
+print("There you go" + Enxhi + ", three years ago you were" + str(z) + "years old")
