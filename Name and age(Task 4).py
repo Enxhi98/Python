dd795f8b70053ded06d34687b5dcf51be2ad263b
Enxhi98 <21321344@student.uwl.ac.uk>
2016-10-20 10:10:03 +0100

    Update Name and age(Task 4).py

--- Name and age(Task 4).py
+++ Name and age(Task 4).py
@@ -1,5 +1,5 @@
 # Write a program that asks for your name and age and shows what your age will be next year
-›print("Hey, what is your name?")
+print("Hey, what is your name?")
 Enxhi = input("Hey what is your name?")
 x = int
 x = int(input("And how old are you?"))
