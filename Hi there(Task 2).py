02a65688643c87f45e1bd1b3e640ee776d7aac53
Enxhi98 <21321344@student.uwl.ac.uk>
2016-10-20 10:35:27 +0100

    Update Hi there(Task 2).py

--- Hi there(Task 2).py
+++ Hi there(Task 2).py
@@ -1,5 +1,8 @@
+# Write a pprogram that asks for your name and greets you
 print("Hi there, what is your name?")
 Enxhi=input("Hi there, what is your name?")
 print("Hello" + Enxhi )
 print("How are you?")
+Fine_thank_you=input("How are you?") # I decided to extend the program more 
+print("I am very pleased to hear that" + Enxhi)
     
