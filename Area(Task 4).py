ae75ea76e70af576fa74d764cdb11f22ea839c39
Enxhi98 <21321344@student.uwl.ac.uk>
2016-10-21 12:21:31 +0100

    Update Area(Task 4).py

--- Area(Task 4).py
+++ Area(Task 4).py
@@ -1,3 +1,6 @@
+#Given that radius is 2 and area is calculated as 12.5678, use string format operators to print 
+#the values of the variables radius and area so that the output looks like this
+#import math fucntion
 import math
 radius = int(input("Radius: "))
 area = math.pi*(radius ** 2)
