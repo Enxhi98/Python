127520b080ff86cf37cf275886df7988759b0805
Enxhi98 <21321344@student.uwl.ac.uk>
2016-10-22 20:35:29 +0100

    Update Area(Task 4).py

--- Area(Task 4).py
+++ Area(Task 4).py
@@ -1,5 +1,4 @@
-#Given that radius is 2 and area is calculated as 12.5678, 
-#use string format operators to print the values of the variables radius and area so that the output looks like this
+#Give the circle's radius a value and calculate its area.
 #import math fucntion
 import math
 radius = int(input("Radius: "))
@@ -7,7 +6,7 @@ area = math.pi*(radius ** 2)
 print("The area of the circle is: \t%.2f" %area)
 
 
-#Given a rectangle with width as 2.5 cm and length as 5 cm, calculate the area of the rectangle
+#Give the rectangle a width and length value, calculate the area of the rectangle.
 import math
 width = float(input("Please enter the width of a rectangle: "))
 length = float(input("Please enter the height of a rectangle: "))
@@ -16,7 +15,7 @@ Area = width * height
 print("\n Area of a Rectangle is: %.2f" %Area)
 
 
-#Given a square with side as 7 cm, calculate its perimeter and area
+#Give the side a value and calculate the square's perimeter and area.
 import math
 slength = int(input("Enter side length of square: "))
 print("What is the perimeter of the square?")
