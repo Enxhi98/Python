77ed3e4c0ac7b21d4d752a6e26934027004f6c1f
Enxhi98 <21321344@student.uwl.ac.uk>
2016-10-22 19:45:50 +0100

    Update Area(Task 4).py

--- Area(Task 4).py
+++ Area(Task 4).py
@@ -1,5 +1,5 @@
-#Given that radius is 2 and area is calculated as 12.5678, use string format operators to print 
-#the values of the variables radius and area so that the output looks like this
+"""Given that radius is 2 and area is calculated as 12.5678, 
+use string format operators to print the values of the variables radius and area so that the output looks like this"""
 #import math fucntion
 import math
 radius = int(input("Radius: "))
