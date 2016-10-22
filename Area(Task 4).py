ace1a45a8f994540350954bd7a2130adfd26a799
Enxhi98 <21321344@student.uwl.ac.uk>
2016-10-22 20:15:05 +0100

    Update Area(Task 4).py

--- Area(Task 4).py
+++ Area(Task 4).py
@@ -1,7 +1,17 @@
-"""Given that radius is 2 and area is calculated as 12.5678, 
-use string format operators to print the values of the variables radius and area so that the output looks like this"""
+#Given that radius is 2 and area is calculated as 12.5678, 
+#use string format operators to print the values of the variables radius and area so that the output looks like this
 #import math fucntion
 import math
 radius = int(input("Radius: "))
 area = math.pi*(radius ** 2)
 print("The area of the circle is: \t%.2f" %area)
+
+
+#Given a rectangle with width as 2.5 cm and length as 5 cm, calculate the area of the rectangle
+import math
+width = float(input('Please Enter the Width of a Rectangle: '))
+length = float(input('Please Enter the Height of a Rectangle: '))
+#calculate the area
+Area = width * height
+print("\n Area of a Rectangle is: %.2f" %Area)
+        
