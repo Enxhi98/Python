7107ae40451b28a756bf5a307bb2be3b09749989
Enxhi98 <21321344@student.uwl.ac.uk>
2016-10-22 19:59:50 +0100

    Update Name and age(Task 4).py

--- Name and age(Task 4).py
+++ Name and age(Task 4).py
@@ -5,7 +5,14 @@ x = int
 x = int(input("And how old are you?"))
 y = int(x + 1)
 print("Hello" + Enxhi + ",next year you will be" + str(y) + "years old!")
-print("Oh thank you. Can you tell me how old was I three years ago?") # I added another question regarding my age three years ago this time using difference
+print("Oh thank you. Can you tell me how old was I three years ago?")
 x= int(input("Yes of course. Could you tell me your age again?"))
 z = int(x - 3)
 print("There you go" + Enxhi + ", three years ago you were" + str(z) + "years old")
+Yes_a_sister = input("Do you have any sisters or brothers?")
+print("Do you have any sisters or brothers?")
+x = 18
+y = int(input("How old is she?"))
+z = int(x - y)
+print("Your sister is" + str(z) + "years younger than you")
+print("Thank you so much !")
